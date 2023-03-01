e2ee03131e626a87f14dd699e443dfb6858030fa
osita chisom <ositachisom7@gmail.com>
2023-03-01 22:28:46 +0100

    second commit

--- app.js
+++ app.js
@@ -73,7 +73,6 @@ passport.deserializeUser(function(id, done) {
   });
 });
 
-
 app.get("/", function(req, res) {
   res.render("home");
 });
@@ -191,6 +190,6 @@ app.post("/customer", function(req, res) {
 });
 
 
-app.listen(process.env.PORT || 3000, function() {
+app.listen(process.env.PORT || 5000, function() {
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
